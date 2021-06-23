eef1d8cf7e5e1f6951fdc373afa877a6bd1641e1
SeoDongWoo1216 <dw6642@naver.com>
2021-06-23 17:33:52 +0900

    21-06-23 MRP 윈폼(데이터센싱받기, DB연동) 커밋

---
+++ DeviceSubApp/FrmMain.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace DeviceSubApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtConnectionString = new System.Windows.Forms.TextBox();
+            this.TxtClientID = new System.Windows.Forms.TextBox();
+            this.TxtSubscriptionTopic = new System.Windows.Forms.TextBox();
+            this.RtbSubscr = new System.Windows.Forms.RichTextBox();
+            this.BtnDisconnect = new System.Windows.Forms.Button();
+            this.BtnConnect = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.LblResult = new System.Windows.Forms.ToolStripStatusLabel();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(18, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connection String";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(87, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Client ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(12, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Subscription Topic";
+            // 
+            // TxtConnectionString
+            // 
+            this.TxtConnectionString.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TxtConnectionString.Location = new System.Drawing.Point(166, 22);
+            this.TxtConnectionString.Name = "TxtConnectionString";
+            this.TxtConnectionString.Size = new System.Drawing.Size(472, 26);
+            this.TxtConnectionString.TabIndex = 1;
+            this.TxtConnectionString.Text = "127.0.0.1";
+            // 
+            // TxtClientID
+            // 
+            this.TxtClientID.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TxtClientID.Location = new System.Drawing.Point(166, 60);
+            this.TxtClientID.Name = "TxtClientID";
+            this.TxtClientID.Size = new System.Drawing.Size(472, 26);
+            this.TxtClientID.TabIndex = 1;
+            this.TxtClientID.Text = "SUBSCR01";
+            // 
+            // TxtSubscriptionTopic
+            // 
+            this.TxtSubscriptionTopic.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TxtSubscriptionTopic.Location = new System.Drawing.Point(166, 98);
+            this.TxtSubscriptionTopic.Name = "TxtSubscriptionTopic";
+            this.TxtSubscriptionTopic.Size = new System.Drawing.Size(472, 26);
+            this.TxtSubscriptionTopic.TabIndex = 1;
+            this.TxtSubscriptionTopic.Text = "factory1/machine1/data/";
+            // 
+            // RtbSubscr
+            // 
+            this.RtbSubscr.Location = new System.Drawing.Point(12, 132);
+            this.RtbSubscr.Name = "RtbSubscr";
+            this.RtbSubscr.Size = new System.Drawing.Size(1160, 496);
+            this.RtbSubscr.TabIndex = 3;
+            this.RtbSubscr.Text = "";
+            // 
+            // BtnDisconnect
+            // 
+            this.BtnDisconnect.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnDisconnect.Location = new System.Drawing.Point(1045, 83);
+            this.BtnDisconnect.Name = "BtnDisconnect";
+            this.BtnDisconnect.Size = new System.Drawing.Size(127, 43);
+            this.BtnDisconnect.TabIndex = 4;
+            this.BtnDisconnect.Text = "Disconnection";
+            this.BtnDisconnect.UseVisualStyleBackColor = true;
+            this.BtnDisconnect.Click += new System.EventHandler(this.BtnDisconnect_Click);
+            // 
+            // BtnConnect
+            // 
+            this.BtnConnect.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnConnect.Location = new System.Drawing.Point(912, 83);
+            this.BtnConnect.Name = "BtnConnect";
+            this.BtnConnect.Size = new System.Drawing.Size(127, 43);
+            this.BtnConnect.TabIndex = 5;
+            this.BtnConnect.Text = "Connection";
+            this.BtnConnect.UseVisualStyleBackColor = true;
+            this.BtnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LblResult});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 639);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1184, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // LblResult
+            // 
+            this.LblResult.Name = "LblResult";
+            this.LblResult.Size = new System.Drawing.Size(14, 17);
+            this.LblResult.Text = "0";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.BtnConnect);
+            this.Controls.Add(this.BtnDisconnect);
+            this.Controls.Add(this.RtbSubscr);
+            this.Controls.Add(this.TxtSubscriptionTopic);
+            this.Controls.Add(this.TxtClientID);
+            this.Controls.Add(this.TxtConnectionString);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.Text = "IoT Device Subscriber";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtConnectionString;
+        private System.Windows.Forms.TextBox TxtClientID;
+        private System.Windows.Forms.TextBox TxtSubscriptionTopic;
+        private System.Windows.Forms.RichTextBox RtbSubscr;
+        private System.Windows.Forms.Button BtnDisconnect;
+        private System.Windows.Forms.Button BtnConnect;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel LblResult;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
