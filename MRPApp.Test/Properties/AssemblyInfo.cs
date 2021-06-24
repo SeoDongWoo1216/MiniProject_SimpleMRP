88b74ce7cd98bb069a6d651d4b7caf68aad11b47
SeoDongWoo1216 <dw6642@naver.com>
2021-06-24 17:28:10 +0900

    0624 MRPAPP 단위 테스트 생성(실패..)

---
+++ MRPApp.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MRPApp.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MRPApp.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("57f53953-cdd3-40c3-a368-49030c46811b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MRPApp.Test/SettingsTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace MRPApp.Test
+{
+    [TestClass]
+    public class SettingsTest
+    {
+        [TestMethod]
+        public void IsDuplicateDataTest()
+        {
+            var inputCode = "PC10001";   // DB상에 있는 값
+            var expectVal = true;        // 예상값
+
+            var code = Logic.DataAccess.GetSettings().Where(d => d.BasicCode.Contains(inputCode));
+            var readVal = code != null ? true : false;
+
+            Assert.AreEqual(expectVal, readVal);  // 값이 같으면 Pass, 다르면 Fail
+        }
+
+        [TestMethod]
+        public void IsCodeSearched()
+        {
+            var expectVal = 2; // 예상값
+            var inputCode = "설비";
+
+            var realVal = Logic.DataAccess.GetSettings().Where(d => d.CodeName.Contains(inputCode)).Count();
+        }
+
+        [TestMethod]
+        public void IsEmailCorrect()
+        {
+            var inputEmail = "이메일";
+            Assert.IsTrue(Commons.IsValidEmail(inputEmail));
+        }
+    }
+}
