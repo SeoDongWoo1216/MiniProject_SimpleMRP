8b8bbb25a4496a0b07bd66c800426bc5614f3cf8
SeoDongWoo1216 <dw6642@naver.com>
2021-06-24 15:16:21 +0900

    06-24 WPF 설정화면 개발 완료

--- MRPApp/Logic/DataAccess.cs
+++ MRPApp/Logic/DataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MRPApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using System.Linq;
@@ -9,6 +10,34 @@ namespace MRPApp.Logic
 {
     public class DataAccess
     {
-       
+        // 세팅 테이블에서 데이터 가져오기
+        internal static List<Settings> GetSettings()
+        {
+            List<Model.Settings> settings;
+
+            using (var ctx = new MRPEntities())
+                settings = ctx.Settings.ToList();  // 세팅즈에있는 DB 데이터를 가져와서 리스트로 만들겠다. (SELECT)
+
+            return settings;  // DB데이터가 담긴 리스트(settings)를 반환
+        }
+
+        internal static int Setsettings(Settings item)  // object -> int로 바꿔줬음
+        {
+            using (var ctx = new MRPEntities())
+            {
+                ctx.Settings.AddOrUpdate(item);  // Insert or Update = AddOrUpdate 임. (데이터 삽입)
+                return ctx.SaveChanges();        // COMMIT
+            }
+        }
+
+        internal static int DelSettings(Settings item)
+        {
+            using(var ctx = new MRPEntities())
+            {
+                var obj = ctx.Settings.Find(item.BasicCode);  // 삭제할 데이터를 검색해서 그 검색된 데이터를 삭제함
+                ctx.Settings.Remove(obj);     // obj를 Delete = Remove (데이터 삭제)
+                return ctx.SaveChanges();
+            }
+        }
     }
 }
